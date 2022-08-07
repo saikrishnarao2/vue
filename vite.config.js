ab752ab5fea62c8b3d897ff8b9612c7a05adbeab
saikrishnarao2 <saikrishnarao2@gmail.com>
2022-08-07 22:04:48 +0530

    Fixed assets path

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/todo-list-app/',
+  base: '/',
   plugins: [vue()],
   resolve: {
     alias: {
